37e4bf75458ad6bc0dbc69618a2497c0ed0543ea
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-19 21:47:44 +0300

    fin

--- index.ts
+++ index.ts
@@ -3,11 +3,12 @@ import { take, multicast, tap, mapTo } from 'rxjs/operators';
 
 // https://www.learnrxjs.io/learn-rxjs/operators/multicasting/multicast
 
+// Example 2: multicast with ReplaySubject
 //emit every 2 seconds, take 5
-const source = interval(2000).pipe(take(5));
+const srcInterval = interval(2000).pipe(take(5));
 
 //example with ReplaySubject
-const example = source.pipe(
+const example = srcInterval.pipe(
   //since we are multicasting below, side effects will be executed once
   tap((_) => console.log('Side Effect #2')),
   mapTo('Result Two!')
