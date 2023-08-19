f002246ebb13c1fc7edd08c67773949341b17b91
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-19 21:40:28 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RXJS-multicast-ReplaySubject-2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-lxgjb2)
---
+++ index.ts
@@ -0,0 +1,26 @@
+import { interval, ReplaySubject } from 'rxjs';
+import { take, multicast, tap, mapTo } from 'rxjs/operators';
+
+// https://www.learnrxjs.io/learn-rxjs/operators/multicasting/multicast
+
+//emit every 2 seconds, take 5
+const source = interval(2000).pipe(take(5));
+
+//example with ReplaySubject
+const example = source.pipe(
+  //since we are multicasting below, side effects will be executed once
+  tap((_) => console.log('Side Effect #2')),
+  mapTo('Result Two!')
+);
+//can use any type of subject
+const multi = example.pipe(multicast(() => new ReplaySubject(5)));
+//subscribe subject to source
+multi.connect();
+
+setTimeout(() => {
+  /*
+   subscriber will receieve all previous values on subscription because
+   of ReplaySubject
+   */
+  const subscriber = multi.subscribe((val) => console.group(val));
+}, 5000);
